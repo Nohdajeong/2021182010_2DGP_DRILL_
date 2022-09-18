aa65d7740ef557800bde51f067536e94b315e884
Nohdajeong <dajeong0404@naver.com>
2022-09-18 18:08:29 +0900

    drill4 완성
    2021182010 노다정

---
+++ 04/drill4.py
@@ -0,0 +1,76 @@
+import turtle
+
+turtle.penup()
+turtle.goto(-250,250)
+turtle.pendown()
+
+
+count1 = 5
+while (count1 > 0):
+    count2 = 4
+    while(count2 > 0):
+         turtle.forward(100)
+         turtle.right(90)
+         count2-=1
+    turtle.forward(100)
+    count1-=1
+    
+
+turtle.penup()
+turtle.goto(-250,150)
+turtle.pendown()
+
+count1 = 5
+while (count1 > 0):
+    count2 = 4
+    while(count2 > 0):
+         turtle.forward(100)
+         turtle.right(90)
+         count2-=1
+    turtle.forward(100)
+    count1-=1
+
+    
+turtle.penup()
+turtle.goto(-250,50)
+turtle.pendown()
+
+count1 = 5
+while (count1 > 0):
+    count2 = 4
+    while(count2 > 0):
+         turtle.forward(100)
+         turtle.right(90)
+         count2-=1
+    turtle.forward(100)
+    count1-=1
+
+
+turtle.penup()
+turtle.goto(-250,-50)
+turtle.pendown()
+
+count1 = 5
+while (count1 > 0):
+    count2 = 4
+    while(count2 > 0):
+         turtle.forward(100)
+         turtle.right(90)
+         count2-=1
+    turtle.forward(100)
+    count1-=1
+
+
+turtle.penup()
+turtle.goto(-250,-150)
+turtle.pendown()
+
+count1 = 5
+while (count1 > 0):
+    count2 = 4
+    while(count2 > 0):
+         turtle.forward(100)
+         turtle.right(90)
+         count2-=1
+    turtle.forward(100)
+    count1-=1
